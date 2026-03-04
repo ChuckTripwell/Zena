fb52d73f3748a79b11bee788739b57bf11400ae8
JianZcar <esteban.jianzcar@outlook.com>
2026-03-04 11:15:24 +0800

    added virtualization packages.

---
+++ build-scripts/modules/integrations/virtualization.sh
@@ -0,0 +1,17 @@
+set -ouex pipefail
+
+shopt -s nullglob
+
+dnf5 -y install qemu-kvm libvirt virt-install guestfs-tools
+
+system_services=(
+  libvirtd
+)
+
+systemctl enable "${system_services[@]}"
+preset_file="/usr/lib/systemd/system-preset/01-zena.preset"
+touch "$preset_file"
+
+for service in "${system_services[@]}"; do
+  echo "enable $service" >> "$preset_file"
+done
