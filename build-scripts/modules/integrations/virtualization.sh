280f98271cc0c7ebc253075be5c67ddef5027801
Jian Z'car Esteban <esteban.jianzcar@outlook.com>
2026-03-05 17:49:47 +0800

    Update virtualization.sh

--- build-scripts/modules/integrations/virtualization.sh
+++ build-scripts/modules/integrations/virtualization.sh
@@ -2,17 +2,4 @@ set -ouex pipefail
 
 shopt -s nullglob
 
-dnf5 -y install qemu-kvm libvirt virt-install guestfs-tools waydroid
-
-system_services=(
-  libvirtd
-  waydroid-container
-)
-
-systemctl enable "${system_services[@]}"
-preset_file="/usr/lib/systemd/system-preset/01-zena.preset"
-touch "$preset_file"
-
-for service in "${system_services[@]}"; do
-  echo "enable $service" >> "$preset_file"
-done
+dnf5 -y install qemu-kvm libvirt virt-install guestfs-tools virt-manager waydroid
